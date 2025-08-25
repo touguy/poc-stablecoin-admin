f8ebabe4b920e94940b7d7b19e6318c1a89e0dcb
92kyoung <ddowei92@naver.com>
2025-08-25 16:08:40 +0900

    feat: context -> zustand로 대체

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,50 +1,56 @@
 // src/hooks/useLogin.ts
-import { useAuth } from '@/context/AuthContext';
-import { api } from '@/lib/axios';
-import { jwtDecode } from 'jwt-decode';
-import { useCallback, useState } from 'react';
 
-type LoginReq = { username: string; password: string };
-type LoginRes = { user: { id: string; username: string }, jwt: string };
+import { api } from "@/lib/axios";
+import { useAuthStore } from "@/stores/authStore";
+import { LoginReq } from "@/types/auth";
+import { jwtDecode } from "jwt-decode";
+import { useCallback, useState } from "react";
+
+type LoginRes = { user: { id: string; username: string }; jwt: string };
 interface DecodedToken {
   userid: string;
   address: string;
 }
 
 export function useLogin() {
-  const { setUser } = useAuth();
+  const { setUser } = useAuthStore(); 
+
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const login = useCallback(async (payload: LoginReq) => {
-    setLoading(true);
-    setError(null);
-    try {
-      const data = await api.post<LoginRes>('/auth/login', payload);
-      setUser({
-        id: data.data.user.id,
-        username: data.data.user.username
-      });
-      // JWT 저장
-      localStorage.setItem('token', data.data.jwt);
-      localStorage.setItem('userId', data.data.user.id);
-      localStorage.setItem('username', data.data.user.username);
-      // jwt decoding
-      const jwtPayload: DecodedToken = jwtDecode(data.data.jwt);
-      
-      localStorage.setItem('address', jwtPayload.address);
+  const login = useCallback(
+    async (payload: LoginReq) => {
+      setLoading(true);
+      setError(null);
 
-      return {
-        id: data.data.user.id,
-        address: data.data.user.username
-      };
-    } catch (e: any) {
-      setError(e.message ?? '로그인에 실패했습니다.');
-      throw e;
-    } finally {
-      setLoading(false);
-    }
-  }, [setUser]);
+      try {
+        const data = await api.post<LoginRes>("/auth/login", payload);
+        setUser({
+          id: data.data.user.id,
+          username: data.data.user.username,
+        });
+        // JWT 저장
+        localStorage.setItem("token", data.data.jwt);
+        localStorage.setItem("userId", data.data.user.id);
+        localStorage.setItem("username", data.data.user.username);
+        // jwt decoding
+        const jwtPayload: DecodedToken = jwtDecode(data.data.jwt);
+
+        localStorage.setItem("address", jwtPayload.address);
+
+        return {
+          id: data.data.user.id,
+          address: data.data.user.username,
+        };
+      } catch (e: any) {
+        setError(e.message ?? "로그인에 실패했습니다.");
+        throw e;
+      } finally {
+        setLoading(false);
+      }
+    },
+    [setUser]
+  );
 
   const logout = useCallback(async () => {
     setLoading(true);
@@ -52,12 +58,12 @@ export function useLogin() {
     try {
       // await api.post('/auth/logout');
       setUser(null);
-      localStorage.removeItem('token');
-      localStorage.removeItem('userId');
-      localStorage.removeItem('username');
-      localStorage.removeItem('address');
+      localStorage.removeItem("token");
+      localStorage.removeItem("userId");
+      localStorage.removeItem("username");
+      localStorage.removeItem("address");
     } catch (e: any) {
-      setError(e.message ?? '로그아웃에 실패했습니다.');
+      setError(e.message ?? "로그아웃에 실패했습니다.");
       throw e;
     } finally {
       setLoading(false);
@@ -72,7 +78,7 @@ export function useLogin() {
         address: decoded.address,
       };
     } catch (error) {
-      console.error('Failed to decode token:', error);
+      console.error("Failed to decode token:", error);
       return null;
     }
   };
