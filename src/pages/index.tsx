fe685e0c435dd6eba76c2344e8f44409c4c429c2
92kyoung <ddowei92@naver.com>
2025-08-25 18:24:23 +0900

    remove: 미사용 로직 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,28 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-
 export default function Home() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태 관리
-  const router = useRouter();
-
-  useEffect(() => {
-    // JWT 토큰 확인 (예: localStorage에서 가져오기)
-    const token = sessionStorage.getItem("jwt"); // 세션 스토리지에서 JWT 가져오기
-    console.log("token", token);
-    if (token) {
-      setIsLoggedIn(true); // 토큰이 있으면 로그인 상태로 설정
-    }
-  }, [router]);
-
-  
   return (
     <div className="max-w-2xl mx-auto mt-16 p-8 bg-white rounded shadow">
-      <h1 className="text-2xl font-bold mb-6 text-center">스테이블 코인 메인 페이지</h1>
+      <h1 className="text-2xl font-bold mb-6 text-center">
+        스테이블 코인 메인 페이지
+      </h1>
       <ul className="space-y-6">
         <li>
           <span className="font-semibold text-blue-700">1. 관리자 페이지</span>
           <div className="text-gray-700 ml-2">
-            관리자 페이지에서는 스테이블 코인의 발행 및 환불 신청을 관리할 수 있습니다.<br />
-            좌측 메뉴를 통해 발행 신청, 환불 신청, 주소록 관리 등의 기능을 이용할 수 있습니다.
+            관리자 페이지에서는 스테이블 코인의 발행 및 환불 신청을 관리할 수
+            있습니다.
+            <br />
+            좌측 메뉴를 통해 발행 신청, 환불 신청, 주소록 관리 등의 기능을
+            이용할 수 있습니다.
           </div>
         </li>
         <li>
           <span className="font-semibold text-blue-700">2. 사용자 페이지</span>
           <div className="text-gray-700 ml-2">
-            사용자 페이지에서는 스테이블 코인을 안전하게 전송하고 관리할 수 있습니다.<br />
-            지갑 연동 후, 네이티브 토큰을 다른 주소로 전송하거나 스마트 컨트랙트를 배포할 수 있습니다.
+            사용자 페이지에서는 스테이블 코인을 안전하게 전송하고 관리할 수
+            있습니다.
+            <br />
+            지갑 연동 후, 네이티브 토큰을 다른 주소로 전송하거나 스마트
+            컨트랙트를 배포할 수 있습니다.
           </div>
         </li>
       </ul>
