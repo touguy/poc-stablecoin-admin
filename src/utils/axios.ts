2d7c099d7acdbb65c6c7c30d06f51217963f8187
92kyoung <ddowei92@naver.com>
2025-08-25 17:59:27 +0900

    feat: axios 파일 위치 변경

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,48 +1,26 @@
-import axios from "axios";
+// src/lib/axios.ts
+import axios from 'axios';
 
-// axios 인스턴스 생성
-const axiosInstance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/stddic`,
-  headers: {
-    "Content-Type": "application/json;charset=utf-8",
-    "Access-Control-Allow-Origin": "*",
-  },
-  withCredentials: false, // 크로스 도메인 인증 관련 설정
+export const api = axios.create({
+    baseURL: 'http://localhost:8080',
+    withCredentials: true,            // 쿠키 기반 세션 사용 시
+    timeout: 15_000,
+    headers: { 
+        'Content-Type': 'application/json',
+        
+    },
 });
 
-//요청 인터셉터 추가
-// axiosInstance.interceptors.request.use(
-//   config => {
-//     startLoading() // 로딩 시작
-//     return config
-//   },
-//   error => {
-//     stopLoading() // 에러 발생 시 로딩 종료
-//     return Promise.reject(error)
-//   },
-// )
-
-// 응답 인터셉터 추가
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
+// 응답 인터셉터 (data 바로 리턴)
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    // 서버 에러 메시지 포맷에 맞게 가공
+    const msg =
+      err?.response?.data?.message ??
+      err?.response?.data ??
+      err?.message ??
+      '요청 중 오류가 발생했습니다.';
+    return Promise.reject(new Error(msg));
   },
-  (error) => {
-    // 에러 핸들링
-    const errorMessage =
-      error.response?.data?.message || "An unexpected error occurred.";
-    const customErrorCallback = error.config?.errorCallback;
-    // 사용자 정의 에러 콜백이 존재할 시
-    if (customErrorCallback) {
-      customErrorCallback(error);
-    } else {
-      //네트워크 오류 시
-      if (!error.response) {
-        console.error("Network or server error", error);
-      }
-      return Promise.reject(error);
-    }
-  }
 );
-
-export default axiosInstance;
--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,4 +1,4 @@
-import { api } from "@/lib/axios";
+import { api } from "@/utils/axios";
 import { AxiosRequestConfig } from "axios";
 
 interface CustomAxiosRequestConfig extends AxiosRequestConfig {
