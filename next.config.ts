54e5b250de959860b3fd32348a7cec9369d19d83
92kyoung <ddowei92@naver.com>
2025-09-02 15:01:26 +0900

    feat: 퍼블리싱 파일 추가

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  basePath: '/admin',
-  output: 'standalone',
+  basePath: "/admin",
+  output: "standalone",
   eslint: {
     ignoreDuringBuilds: true, // ✅ build 단계에서 ESLint 무시
   },
-
+  sassOptions: {
+    includePaths: ["./src/styles"],
+  },
 };
 
 export default nextConfig;
