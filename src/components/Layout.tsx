05c92b37c7c5c568279c868176ba592ee321db5f
92kyoung <ddowei92@naver.com>
2025-09-02 15:22:55 +0900

    feat: 레이아웃 퍼블리싱 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,23 +1,27 @@
+import {
+  Box,
+  Container
+} from '@mui/material';
 import { ReactNode } from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 type Props = {
   children: ReactNode;
-  title: string;
+  isLayout: boolean;
 };
 
-const Layout = ({ children, title }: Props) => {
+const Layout = ({ children, isLayout }: Props) => {
   return (
-    <div className="flex flex-col h-screen overflow-hidden">
-      <Header/>
-      <div className="flex flex-1 min-h-0">
-        <Sidebar/>
-        <main className="flex-1 overflow-auto bg-gray-50 p-6 overflow-hidden">
+    <Box data-page>
+      {isLayout && <Header />}
+      <Container data-container>
+        {isLayout && <Sidebar />}
+        <Box component="main">
           {children}
-        </main>
-      </div>
-    </div>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
