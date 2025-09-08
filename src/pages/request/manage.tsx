1583b21c8c9786cf29406c493a41df8c1ac76c7b
92kyoung <ddowei92@naver.com>
2025-09-08 15:07:29 +0900

    feat: 발행/환불 관리 페이지 레이아웃 상태 추가

--- src/pages/request/manage.tsx
+++ src/pages/request/manage.tsx
@@ -2,16 +2,16 @@ import BreadcrumbsBox from "@/components/common/BreadcrumbsBox";
 import RequestListCard from "@/components/request/list/RequestListCard";
 import { Stack, Typography } from "@mui/material";
 
-const ManagePage = () => {
+export default function ManagePage() {
   return (
     <>
-      <BreadcrumbsBox menus={["발행/환불 관리", "발행/환불 현황"]} />
+      <BreadcrumbsBox menus={["발행/환불 관리", "발행/환불 관리"]} />
       <Stack data-title-area sx={{ mt: "1.6rem", mb: "1.2rem" }}>
-        <Typography>발행/환불 현황</Typography>
+        <Typography>발행/환불 관리</Typography>
       </Stack>
       <RequestListCard />
     </>
   );
-};
+}
 
-export default ManagePage;
+ManagePage.isLayout = true;
