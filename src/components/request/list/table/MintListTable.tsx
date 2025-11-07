7dac71339fb688d472b8caf54b183611e147f686
92kyoung <ddowei92@naver.com>
2025-11-07 10:39:02 +0900

    feat: 스테이블코인 심볼 상수화

--- src/components/request/list/table/MintListTable.tsx
+++ src/components/request/list/table/MintListTable.tsx
@@ -15,6 +15,7 @@ import { useState } from "react";
 import MuiDataGrid from "../../../common/MuiDataGrid";
 import RequestConfirmCard from "../../confirm/RequestConfirmCard";
 import RequestDetailCard from "../../detail/RequestDetailCard";
+import { STABLECOINS } from "@/constants/stablecoin";
 
 type MintListTableProps = {
   data: any;
@@ -116,7 +117,7 @@ const MintListTable = ({
     { field: "applicantId", headerName: "신청자 ID", width: 130 },
     {
       field: "requestedIssueCount",
-      headerName: "발행 신청 수량(KRWH)",
+      headerName: `발행 신청 수량(${STABLECOINS.SYMBOL})`,
       width: 170,
       align: "right",
     },
